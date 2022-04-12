7bb163a53b9253768d350e349845f6cf630809dd
khalifaOyabun <ababacarkhalifa13@gmail.com>
2022-04-12 04:49:54 +0200

    Premier rendu du projet

---
+++ js/copy.js
@@ -0,0 +1,7 @@
+function copyToClipBoardNum() {
+    navigator.clipboard.writeText("+221 77 636 08 39");
+}
+
+function copyToClipBoardMail() {
+    navigator.clipboard.writeText("ababacarkhalifa13@gmail.com");
+}
